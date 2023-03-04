07c2a1e5a3da29292dc9b2cf0c77c3bafc78a282
richeyATP <richey@avenirtechpartners.com>
2023-03-04 00:09:50 +0000

    calc js

---
+++ main.js
@@ -0,0 +1,73 @@
+var commands = '19 20 + DUP + 60 + +'
+
+function process(stream) {
+
+    let commandsArray = stream.split(' ')
+
+    let stack = new Array()
+    let topOfStack = -1
+
+    let value = 0
+    let valueA = 0
+    let valueB = 0
+
+    for (const token of commandsArray) {
+        if (token == 'POP') {
+        
+            if (topOfStack == -1) return -1
+           
+            stack.pop()
+            topOfStack--
+        
+        } else if (token == 'DUP') {
+        
+            if (topOfStack == -1) return -1
+        
+            value = stack[topOfStack]
+        
+            topOfStack++
+            stack.push(value)
+        
+        } else if (token == '+') {
+        
+            if (topOfStack < 1) return -1
+        
+            valueA = stack[topOfStack]
+            valueB = stack[topOfStack - 1]
+        
+            if (valueA + valueB > 100000 - 1) return -1
+        
+            stack.pop()
+            stack.pop()
+            topOfStack--
+            stack.push(valueA + valueB)
+        
+        } else if (token == '-') {
+        
+            if (topOfStack < 1) return -1
+        
+            valueA = stack[topOfStack]
+            valueB = stack[topOfStack - 1]
+        
+            if (valueA - valueB < 0) return -1
+        
+            stack.pop()
+            stack.pop()
+            topOfStack--
+            stack.push(valueA - valueB)
+        
+        } else {
+        
+            topOfStack++
+            stack.push(+token)
+
+        }
+
+    }
+
+    return stack[topOfStack]
+
+}
+
+var result = process(commands)
+console.log(result)
